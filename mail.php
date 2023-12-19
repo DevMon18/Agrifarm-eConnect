f9fc26e3eb49c4be3ded9124c7c12f0283b0fd04
Michael Jhon Vincent O. Lobo <michaeljhonvincent.lobo@evsu.edu.ph>
2023-12-19 12:14:53 +0800

    Update mail.php

--- mail.php
+++ mail.php
@@ -13,13 +13,13 @@ function sendemail_verify($name, $email, $verify_token){
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'michaeljhonvincent.lobo@evsu.edu.ph';  //SMTP username
-    $mail->Password   = 'Dinagko_Lobo@1432$';                  //SMTP password
+    $mail->Username   = 'Your Gmail address';                   //SMTP username
+    $mail->Password   = 'Your Gmail Password';                  //SMTP password
     $mail->SMTPSecure = "tls";                                  //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('michaeljhonvincent.lobo@evsu.edu.ph', $name);
+    $mail->setFrom('Your Gmail address', $name);
     $mail->addAddress($email);                                  //Name is optional
 
     //Content
